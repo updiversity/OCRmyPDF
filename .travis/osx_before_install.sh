13acd625dbc6252aa48f08bb302b17c9c43fd9b9
James R. Barlow <jim@purplerock.ca>
2017-03-01 23:04:56 -0800

    Fix poppler missing from osx tests

--- .travis/osx_before_install.sh
+++ .travis/osx_before_install.sh
@@ -9,6 +9,7 @@ brew install python3
 brew install libxml2 libffi leptonica
 brew install unpaper   # optional
 brew install tesseract
+brew install poppler  # for testing only
 
 pip3 install --upgrade pip
 pip3 install wheel
